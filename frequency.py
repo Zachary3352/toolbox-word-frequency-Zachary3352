a199a36941cdd13746276a42c972ad4056c5242f
Zachary3352 <thelexingtonian1@gmail.com>
2020-04-16 20:13:45 -0400

    Complete Word Frequency Toolbox

--- frequency.py
+++ frequency.py
@@ -1,7 +1,7 @@
 """Analyze the word frequencies in a book downloaded from Project Gutenberg."""
 
 import string
-
+from collections import OrderedDict
 
 def get_word_list(file_name):
     """Read the specified project Gutenberg book.
@@ -10,7 +10,24 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list. All words are
     converted to lower case.
     """
-    pass
+    lines = []
+    final_lines = []
+
+    with open(file_name, encoding="utf8") as fp:
+        for line in fp:
+            processed_line = line.strip()
+            processed_line = processed_line.lower()
+            processed_line = processed_line.split()
+            lines.append(processed_line)
+
+    for sublist in lines:
+        for item in sublist:
+            temp_string = ""
+            for letter in range(len(item)+1):
+                temp_string = temp_string + item[letter-1:letter].strip(string.punctuation)
+            final_lines.append(temp_string)
+
+    return final_lines
 
 
 def get_top_n_words(word_list, n):
@@ -30,10 +47,22 @@ def get_top_n_words(word_list, n):
     int
         The n most frequently occurring words ordered from most to least.
     frequently to least frequentlyoccurring
+
+    Examples
+    --------
+    >>> print(get_top_n_words("pg32325.txt", 100))
+    ['and', 'the', 'i', 'to', 'a', 'it', 'was', 'of', 'he', 'in', 'you', 'that', 'but', 'so', 'on', 'for', 'all', 'we', 'up', 'out', 'me', 'him', 'they', 'says', 'got', 'then', 'no', 'with', 'there', 'his', 'as', 'them', 'she', 'said', 'see', 'had', 'down', 'well', 'at', 'when', 'about', 'by', 'my', 'if', 'what', 'would', 'do', 'or', 'come', 'be', 'one', 'is', 'her', 'didnt', 'jim', 'dont', 'get', 'time', 'could', 'this', 'right', 'aint', 'went', 'warnt', 'off', 'over', 'go', 'good', 'way', 'like', 'just', 'old', 'around', 'know', 'now', 'de', 'along', 'en', 'its', 'done', 'tom', 'because', 'couldnt', 'back', 'ever', 'your', 'some', 'man', 'why', 'little', 'any', 'never', 'say', 'more', 'how', 'two', 'have', 'again', 'away', 'too']
     """
-    pass
+    words = get_word_list(word_list)
+    hist = dict()
+    for word in words:
+        hist[word] = hist.get(word, 0) + 1
+    sorted_hist = OrderedDict(sorted(hist.items(), key=lambda x: x[1], reverse=True)) # I was curious about which words showed up most in the book, so I used this answer to help me sort the dictionary: https://thomas-cokelaer.info/blog/2017/12/how-to-sort-a-dictionary-by-values-in-python/
+    sorted_hist_list = list(sorted_hist.keys())
+    return sorted_hist_list[0:n]
 
 
 if __name__ == "__main__":
-    print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    import doctest
+    #doctest.testmod() # Run to test this script. If everything is working as expected, you'll see no output.
+    print(get_top_n_words("pg32325.txt", 100))
